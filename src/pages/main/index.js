30c50dc05ee1e12e98ba1bfdc05e1873b8c49831
Guilherme-Ferreira2107 <guilhermeferreira2107@gmail.com>
2021-04-08 12:11:45 -0300

    Add gráfico mockado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { mask } from "remask";
 import { useHistory } from "react-router";
+
+// ApexChart
+import ReactApexChart from "react-apexcharts";
 
 // Components
 import Header from "../../components/header";
@@ -24,6 +26,57 @@ const Main = () => {
   const [loading, setLoading] = useState(false);
   const [addValor, setAddValor] = useState("");
   const [removeValor, setRemoveValor] = useState("");
+  const series = [
+    {
+      name: "Cases",
+      data: [
+        555,
+        12038,
+        69030,
+        88369,
+        167466,
+        932638,
+        2055423,
+        3343777,
+        3845718,
+      ],
+    },
+    {
+      name: "Recovered",
+      data: [28, 284, 9394, 42710, 76026, 191853, 501538, 1029651, 1255481],
+    },
+    {
+      name: "Deaths",
+      data: [17, 259, 1666, 2996, 6472, 49675, 140658, 238619, 269567],
+    },
+  ];
+  const options = {
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "smooth",
+    },
+    xaxis: {
+      type: "datetime",
+      categories: [
+        "1/22/20",
+        "2/1/20",
+        "2/15/20",
+        "3/1/20",
+        "3/15/20",
+        "4/1/20",
+        "4/15/20",
+        "5/1/20",
+        "5/7/20",
+      ],
+    },
+    tooltip: {
+      x: {
+        format: "dd/MM/yyyy",
+      },
+    },
+  };
 
   // Recuperar dados
   const recuperarDados = useCallback(async () => {
@@ -223,7 +276,19 @@ const Main = () => {
             </div>
           </div>
         </Loading>
+        <Loading loading={loading}>
+          <div className="container exhibit">
+            <ReactApexChart
+              options={options}
+              series={series}
+              type="line"
+              height={350}
+              width="100%"
+            />
+          </div>
+        </Loading>
       </div>
+
       <Footer />
       {showAlert && alert}
     </>
